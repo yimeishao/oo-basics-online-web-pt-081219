c493515f3488391369f148218254ceb830d84a3f
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-09-07 18:10:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,5 +11,19 @@ end
 def author 
   @author
 end
+def page_count=(num)
+  @page_count = num
+end
+def page_count
+  @page_count
+end
+def genre=(genre)
+  @genre = genre
 end 
-
+def genre 
+  @genre
+end
+def turn_page
+  puts "Flipping the paage...wow, you read fast!"
+end
+end
